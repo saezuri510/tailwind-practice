6e59013e1bd00dfee52eb1607d78c39c52d26449
saezuri510 <saezuru510@gmail.com>
2022-09-10 20:20:27 +0900

    tailwind練習3

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const activeClassNameStyle =
+    "text-red-600 flex flex-col items-center cursor-pointer group w-12";
+const classNameStyle = "flex flex-col items-center cursor-pointer group w-12";
+
 function NavItem({ Icon, title }) {
     return (
         <NavLink
             to={title.toLowerCase()}
-            className="flex flex-col items-center cursor-pointer group w-12 active:text-red-600"
+            className={({ isActive }) =>
+                isActive ? activeClassNameStyle : classNameStyle
+            }
         >
             <Icon size={30} className="mb-1" />
             <p className="font-semibold opacity-0 group-hover:opacity-100">
