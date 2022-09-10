a3f6a6956ca4c39680ae04407d834b15dcd263c4
saezuri510 <saezuru510@gmail.com>
2022-09-10 19:06:44 +0900

    tailwind練習2

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -5,8 +5,7 @@ function NavItem({ Icon, title }) {
     return (
         <NavLink
             to={title.toLowerCase()}
-            className="flex flex-col items-center cursor-pointer group w-12"
-            activeClassName="text-red-600"
+            className="flex flex-col items-center cursor-pointer group w-12 active:text-red-600"
         >
             <Icon size={30} className="mb-1" />
             <p className="font-semibold opacity-0 group-hover:opacity-100">
